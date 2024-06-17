198e185af7c069110f4f33a1032cc109f8611ee1
Khid Pagna <khidpagna@Khids-MacBook-Pro.local>
2024-06-17 15:49:43 +0700

    integrate sass style

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,70 @@
+// Essectial Import
+import './sass/style.scss';
+import React from 'react';
+
 const app: Element | null = document.querySelector("#app");
 
-if (app !== null && app instanceof Element) {
-  console.log(app);
-}
+type CalendarOptionsObject = {
+  futureMonthCount?: number,
+  pastMonthCount?: number,
+};
 
+class BasicCalendar {
+  // Essential Date
+  protected readonly date: Date = new Date();
+  protected readonly month: number = this.date.getMonth();
+  protected readonly year: number = this.date.getFullYear();
+  // Current Date
+  protected currentMonth: number = this.month;
+  protected currentYear: number = this.year;
+  // Calendar cell count, default is 42.
+  protected readonly cellCount: number = 42;
+  // Essential Settings
+  protected futureMonthCount: number;
+  protected pastMonthCount: number;
+
+  constructor(protected classname?: string, options: CalendarOptionsObject = {}) {
+    const defaultOptions: CalendarOptionsObject = {
+      futureMonthCount: 1,
+      pastMonthCount: 1,
+    };
+    // Use spread operator for default options
+    const { futureMonthCount, pastMonthCount } = { ...defaultOptions, ...options };
+    this.futureMonthCount = futureMonthCount!;
+    this.pastMonthCount = pastMonthCount!;
+  }
+
+  renderCalendar(): boolean {
+    if (this.classname) {
+      const mainElement: Element | null = document.querySelector(this.classname);
+      if (mainElement !== null) return true;
+    }
+    return false;
+  }
+
+  public createPanelBar(): Element {
+    const panel = document.createElement("div");
+    panel.classList.add("calendar-panel");
+    return panel;
+  }
+
+  public createCalendarCell(): Element {
+    const cell = document.createElement("div");
+    return cell;
+  }
+
+  public createCalendarCellContainer(): Element {
+    const cellContainer = document.createElement("div");
+    return cellContainer;
+  }
+
+  public createCalendarFooter(): Element {
+    const footer = document.createElement("div");
+    return footer;
+  }
+
+  createMainContainer(): Element {
+    const container = document.createElement("div");
+    return container;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const path = require("path");
+const webpack = require("webpack");
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: "development",
+  mode: isProduction ? "production" : "development",
   entry: {
     index: "./src/index.ts",
   },
   output: {
-    filename: "[name].js",
+    filename: isProduction ? "[name].[contenthash].js" : "[name].js",
     path: path.resolve(__dirname, "dist"),
+    clean: true, // Ensures the output directory is clean
   },
   devServer: {
     static: "./dist",
+    hot: true,
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: isProduction ? "[name].[contenthash].css" : "[name].css",
+    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Basic Calendar",
       filename: "index.html",
       template: "./src/template.html",
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     }),
   ],
   module: {
@@ -29,9 +43,36 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
     ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
+  optimization: {
+    minimize: isProduction,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+    },
+    runtimeChunk: 'single',
+  },
+  devtool: isProduction ? 'source-map' : 'eval-source-map',
 };
